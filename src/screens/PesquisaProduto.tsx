be2a2cb23bd395a1c1acda0f4ff3c1f883e431e0
jaozinhodagarapa <joaovictorribeirooliveira49@gmail.com>
2024-05-16 12:47:49 -0300

    Atualizado

--- src/screens/PesquisaProduto.tsx
+++ src/screens/PesquisaProduto.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FlatList, Image,StatusBar,StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Head from "../components/Head";
 import Footer from "../components/Footer";
+import { useNavigation } from "@react-navigation/native";
 
 
 interface Produto {
@@ -14,6 +15,8 @@ interface Produto {
 
 
 function PesquisaProduto(): React.JSX.Element {
+
+    const navigation = useNavigation();
 
     const produtos: Produto[] = [
         {
@@ -32,9 +35,15 @@ function PesquisaProduto(): React.JSX.Element {
         },
     ]
 
+    const selecionaProduto = (produto: Produto) => {
+        navigation.navigate('EditarProdutos', {produto});
+    }
+
     const renderItem = ({item}: {item:Produto}) => {
         return (
-            <TouchableOpacity style={styles.menuItem}>
+            <TouchableOpacity style={styles.menuItem}
+                onPress={() => selecionaProduto(item)}
+            >
                 <Image source={require('../assets/images/hamburger.png')} style={styles.image}/>
                 <View style={styles.itemDetails}>
                     <Text style={styles.name}>{item.nome}</Text>
